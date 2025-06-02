c2a2c1ed44e67d04676ddc3fe160f66c7a459148
JaMagik <114476897+JaMagik@users.noreply.github.com>
2025-06-02 12:56:49 +0200

    S

--- api/trelloAuth/startFront.jsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-export default function StartAuth() {
-  useEffect(() => {
-    window.location.href = "/api/start";
-  }, []);
-  return <div>Trwa przekierowanie do autoryzacji Trello...</div>;
-}
